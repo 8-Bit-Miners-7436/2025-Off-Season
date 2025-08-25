7b8dcc75ccca231b5c7bebd1edcf235a5fa28668
Shoddy <139808151+ShoddyShoe@users.noreply.github.com>
2025-08-25 12:52:18 -0600

    Cleaned up comments

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,24 +5,20 @@ import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
 
-import com.ctre.phoenix6.hardware.Pigeon2;
-
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.math.geometry.Pose3d;
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+
 public class Robot extends LoggedRobot {
-  boolean BABYMODE = false;
-  // Initialize Xbox Controller, Elevator, and IMU (Gyro)
+  boolean BABYMODE = true;
+  // Initialize Xbox Controller and IMU (Gyro)
   private final XboxController gamepad = new XboxController(0);
-  private final Pigeon2 pigeon = new Pigeon2(10);
-  private Pose3d llTranslationData;
-  private double[] llRotationData;
+  // private final Pigeon2 pigeon = new Pigeon2(10);
+  // private Pose3d llTranslationData;
+  // private double[] llRotationData;
 
   public Robot() {
     // Initialize AdvantageKit (Logger)
@@ -30,11 +26,11 @@ public class Robot extends LoggedRobot {
     Logger.start();
 
     // Start the camera stream
-    UsbCamera camera = CameraServer.startAutomaticCapture();
+    // UsbCamera camera = CameraServer.startAutomaticCapture();
 
     // Configure The Camera
-    camera.setFPS(30);
-    camera.setBrightness(20);
+    // camera.setFPS(30);
+    // camera.setBrightness(20);
   }
 
   // Create Kinematics Object w/ Module Offsets from Robot Center (in meters)
@@ -43,7 +39,7 @@ public class Robot extends LoggedRobot {
     new Translation2d(0.34, -0.3),  // Front Right
     new Translation2d(-0.34, 0.3),  // Rear Left
     new Translation2d(-0.34, -0.3)  // Rear Right
-    );
+  );
 
   // Package Initialized Swerve Modules in an Array for Easy Access
   private final CustomSwerveModule[] modules = new CustomSwerveModule[] {
@@ -55,10 +51,11 @@ public class Robot extends LoggedRobot {
 
   @Override
   public void robotPeriodic() {
-    llTranslationData = LimelightHelpers.getTargetPose3d_CameraSpace("limelight");
-    llRotationData = LimelightHelpers.getCameraPose_TargetSpace("limelight");
-    Logger.recordOutput("LimelightData", llTranslationData);
-    Logger.recordOutput("LimelightRotationData", llRotationData);
+    // Send Limelight Diagnostics to Logger
+    // llTranslationData = LimelightHelpers.getTargetPose3d_CameraSpace("limelight");
+    // llRotationData = LimelightHelpers.getCameraPose_TargetSpace("limelight");
+    // Logger.recordOutput("LimelightData", llTranslationData);
+    // Logger.recordOutput("LimelightRotationData", llRotationData);
   }
 
   @Override
@@ -67,31 +64,26 @@ public class Robot extends LoggedRobot {
     double vx = -gamepad.getLeftY(); // Forward/backward movement
     double vy = -gamepad.getLeftX(); // Strafing movement
     double w = -gamepad.getRightX(); // Rotational movement
-    if (!BABYMODE) {
-      if (LimelightHelpers.getTV("limelight") && gamepad.getStartButton()) {
-        w = 0.03 * llRotationData[4];
-        if (Math.abs(w) < 0.1) w = 0;
-        vy = (Math.abs(llRotationData[4]) > 1) ? 0 : -llTranslationData.getX();
-        vx = (Math.abs(llTranslationData.getX()) > 1) ? 0 : llTranslationData.getY();
-      }
-    } else {
+    if (BABYMODE) {
       vx /= 3;
       vy /= 3;
       w /= 2;
     }
     // Convert Joystick Inputs to Swerve Module Instructions (States)
     ChassisSpeeds chassisSpeeds = new ChassisSpeeds(vx, vy, w);
-    SwerveModuleState[] moduleStates = kinematics.toSwerveModuleStates(
-      ChassisSpeeds.fromFieldRelativeSpeeds(
-        chassisSpeeds,
-        Rotation2d.fromDegrees(pigeon.getYaw().refresh().getValue().magnitude())
-      )
+    SwerveModuleState[] moduleStates;
+    moduleStates = kinematics.toSwerveModuleStates(
+      chassisSpeeds
+      // ChassisSpeeds.fromFieldRelativeSpeeds(
+      //   chassisSpeeds,
+      //   Rotation2d.fromDegrees(pigeon.getYaw().refresh().getValue().magnitude())
+      // )
     );
 
-    SwerveDriveKinematics.desaturateWheelSpeeds(moduleStates, 0.5);
+    // SwerveDriveKinematics.desaturateWheelSpeeds(moduleStates, 0.5);
 
-    // Send Swerve Diagnostics to AdvantageScope
-    Logger.recordOutput("RobotRotation", pigeon.getRotation2d());
+    // Send Swerve Diagnostics to Logger
+    // Logger.recordOutput("RobotRotation", pigeon.getRotation2d());
     Logger.recordOutput("ChassisSpeeds", chassisSpeeds);
     Logger.recordOutput("ModuleStates", moduleStates);
     // Pass Instructions to each Swerve Module
@@ -107,9 +99,9 @@ public class Robot extends LoggedRobot {
       module.resetEncoders();
   }
 
-  @Override
-  public void testInit() {
-    // Zero Out Pigeon to Fix Field-Oriented Drive Alignment
-    pigeon.reset();
-  }
+  // @Override
+  // public void testInit() {
+    // // Zero Out Pigeon to Fix Field-Oriented Drive Alignment
+    // pigeon.reset();
+  // }
 }

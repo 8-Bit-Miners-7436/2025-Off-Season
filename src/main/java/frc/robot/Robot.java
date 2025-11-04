ba56c7dcec0e0074b547a3dba0c508bf372c4a1f
Shoddy <139808151+ShoddyShoe@users.noreply.github.com>
2025-11-04 14:04:43 -0700

    Lots of documentation for new members

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -12,28 +12,41 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 
 
+/*    //  Todo List  //
+* Cleanup/Reformat Code As Needed
+* Implement Basic Pre-Season Bot Functionality
+* Fix Wheel Alignment Issues & Tune PID
+* Add Easy Feature Enable/Disable Options
+* Make Limelight-Powered Auto Goal Alignment
+* See If Input From AdvantageScope Is Possible
+* Figure Out Simulation Software
+*/
+
+
 public class Robot extends LoggedRobot {
-  boolean BABYMODE = true;
-  // Initialize Xbox Controller and IMU (Gyro)
+  // * Initializes Xbox Controller Object
   private final XboxController gamepad = new XboxController(0);
+
+  // * Initializes Pigeon (Velocity/Gyro Info Provider)
   // private final Pigeon2 pigeon = new Pigeon2(10);
-  // private Pose3d llTranslationData;
-  // private double[] llRotationData;
+  // private Pose3d llTargetData;
+  // private double[] llCameraData;
 
   public Robot() {
-    // Initialize AdvantageKit (Logger)
+    // * Starts The Connection To The Logger (AdvantageScope)
     Logger.addDataReceiver(new NT4Publisher());
     Logger.start();
 
-    // Start the camera stream
+    // * Starts The Camera Stream
     // UsbCamera camera = CameraServer.startAutomaticCapture();
 
-    // Configure The Camera
-    // camera.setFPS(30);
-    // camera.setBrightness(20);
+    // * Configures The Camera
+    // camera.setFPS(30);        // Lower FPS To Improve Latency
+    // camera.setBrightness(20); // Boost Brightness
   }
 
-  // Create Kinematics Object w/ Module Offsets from Robot Center (in meters)
+  // * Creates Kinematics Object:
+  // * Stores Wheel Distances From Robot Center (in meters)
   private final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
     new Translation2d(0.34, 0.3),   // Front Left
     new Translation2d(0.34, -0.3),  // Front Right
@@ -41,7 +54,7 @@ public class Robot extends LoggedRobot {
     new Translation2d(-0.34, -0.3)  // Rear Right
   );
 
-  // Package Initialized Swerve Modules in an Array for Easy Access
+  // * Places Initialized Swerve Modules In An Array For Easy Access
   private final CustomSwerveModule[] modules = new CustomSwerveModule[] {
     new CustomSwerveModule(1, "Front Left"),
     new CustomSwerveModule(2, "Front Right"),
@@ -51,57 +64,73 @@ public class Robot extends LoggedRobot {
 
   @Override
   public void robotPeriodic() {
-    // Send Limelight Diagnostics to Logger
-    // llTranslationData = LimelightHelpers.getTargetPose3d_CameraSpace("limelight");
-    // llRotationData = LimelightHelpers.getCameraPose_TargetSpace("limelight");
-    // Logger.recordOutput("LimelightData", llTranslationData);
-    // Logger.recordOutput("LimelightRotationData", llRotationData);
+    // * Records Limelight Diagnostics
+    // llTargetData = LimelightHelpers.getTargetPose3d_CameraSpace("limelight");
+    // llCameraData = LimelightHelpers.getCameraPose_TargetSpace("limelight");
+
+    // * Sends Diagnostics To Logger
+    // ? Descriptions Here Might Be Inaccurate...
+    // Logger.recordOutput("LL Target Data", llTargetData); // Target Rotation & Position In 3D Space
+    // Logger.recordOutput("LL Camera Data", llCameraData); // Camera Position In Relation To Target
   }
 
   @Override
   public void teleopPeriodic() {
-    // Get Joystick Values for Driving
-    double vx = -gamepad.getLeftY(); // Forward/backward movement
-    double vy = -gamepad.getLeftX(); // Strafing movement
-    double w = -gamepad.getRightX(); // Rotational movement
-    if (BABYMODE) {
-      vx /= 3;
-      vy /= 3;
-      w /= 2;
-    }
-    // Convert Joystick Inputs to Swerve Module Instructions (States)
+
+    // * Gets Joystick Values
+    double vx = -gamepad.getLeftY();  // Forward/Backward movement
+    double vy = -gamepad.getLeftX();  // Strafing movement
+    double w  = -gamepad.getRightX(); // Rotational movement
+
+    // * Converts Joystick Inputs Into Directional Movement Data
     ChassisSpeeds chassisSpeeds = new ChassisSpeeds(vx, vy, w);
+
+    // * Decodes Movement Data Into Swerve Module Instructions (States)
     SwerveModuleState[] moduleStates;
     moduleStates = kinematics.toSwerveModuleStates(
-      chassisSpeeds
+
+      // * Default Instructions, Drives Like RC Car
+      chassisSpeeds // ! Comment Out If Enabling Field-Oriented Controls
+
+      // * Implements Field-Oriented Controls Using Pigeon Rotation Data
+      // * Robot Is Controlled From The Driver's Perspective:
+      // * Movement Direction Won't Change Depending On Robot's Rotation
       // ChassisSpeeds.fromFieldRelativeSpeeds(
       //   chassisSpeeds,
       //   Rotation2d.fromDegrees(pigeon.getYaw().refresh().getValue().magnitude())
       // )
     );
 
+    // * Limits Wheel Rotation Speed To Limit Battery Strain
+    // * Uncomment If Experiencing Brownout Issues
     // SwerveDriveKinematics.desaturateWheelSpeeds(moduleStates, 0.5);
 
-    // Send Swerve Diagnostics to Logger
-    // Logger.recordOutput("RobotRotation", pigeon.getRotation2d());
-    Logger.recordOutput("ChassisSpeeds", chassisSpeeds);
-    Logger.recordOutput("ModuleStates", moduleStates);
-    // Pass Instructions to each Swerve Module
+    // * Sends General Driving Diagnostics To Logger
+    // Logger.recordOutput("RobotRotation", pigeon.getRotation2d()); // Pigeon Rotation Data
+    Logger.recordOutput("ChassisSpeeds", chassisSpeeds); // Directional Movement Data
+    Logger.recordOutput("ModuleStates", moduleStates); // Individual Module States
+
+    // * Passes Instructions To Each Swerve Module
     int i = 0;
-    for (SwerveModuleState state : moduleStates)
+    for (SwerveModuleState state : moduleStates) {
       modules[i++].updateState(state);
+    }
   }
 
   @Override
   public void autonomousInit() {
-    // Zero Out all Module Encoders to Fix Alignment Issues
-    for (CustomSwerveModule module : modules)
+    // * Zeros Out Module Encoders To Fix Alignment Issues
+    // * Makes All Modules Believe They Are At 0deg Of Rotation
+    // ? This Shouldn't Have To Be Done, Maybe PID Issue?
+    for (CustomSwerveModule module : modules) {
       module.resetEncoders();
+    }
   }
 
-  // @Override
-  // public void testInit() {
-    // // Zero Out Pigeon to Fix Field-Oriented Drive Alignment
+  @Override
+  public void testInit() {
+    // * Zeros Out Pigeon To Set Field-Oriented Drive Alignment
+    // * Run This When Robot Is Facing Directly Away From Driver
     // pigeon.reset();
-  // }
+  }
 }
--- src/main/java/frc/robot/utils/CustomSwerveModule.java
+++ src/main/java/frc/robot/utils/CustomSwerveModule.java
@@ -20,19 +20,24 @@ public class CustomSwerveModule {
     private final CANcoder steerEncoder;
 
     public CustomSwerveModule(int moduleNumber, String modulePosition) {
-        // Initialize Drive and Steer Motors Based on Module Numbering
+        // * Initializes Module's Drive And Steer Motors
+        // Each Module Has 2 CAN IDs
+        // This Allows Us To Simply Offset The Value By 2 For Each Motor
         driveMotor = new SparkMax(moduleNumber * 2, MotorType.kBrushless);
         steerMotor = new SparkMax(moduleNumber * 2 + 1, MotorType.kBrushless);
 
-        // Initialize the Motor Encoders for Angle and Positioning Feedback
+        // * Initializes The Motor Encoders For Angle And Positioning Feedback
+        // Steer Motors Should Use The Built-In Swerve Casing Encoders
         steerEncoder = new CANcoder(moduleNumber + 10);
         driveEncoder = driveMotor.getEncoder();
 
-        // Initialize PID Controller
+        // * Initializes PID Controller
+        // Todo: Tune PID Values
+        // ? Maybe Try Rev PID Generator
         steerPID = new PIDController(0.5, 0, 0.05);
         steerPID.enableContinuousInput(-0.5, 0.5);
 
-        // Impose Current Limit on Motors to Prevent Brownout
+        // * Impose Soft Current Limit On Motors To Prevent Brownout
         SparkMaxConfig motorConfig = new SparkMaxConfig();
         motorConfig.smartCurrentLimit(30);
         driveMotor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
@@ -40,32 +45,35 @@ public class CustomSwerveModule {
     }
 
     public void updateState(SwerveModuleState newState) {
-        // Get Steer Motor Angle in Rotations
+        // * Get Steer Motor Angle In Rotations
         double steerMotorRot = steerEncoder.getAbsolutePosition().refresh().getValueAsDouble();
 
-        // Fix Wrap Around Issues
+        // * Optimizes Rotation Distance And Removes Wraparound
+        // For Example: If The Motor Is At 180deg And The Driver
+        // Pulls Directly Back On The Stick, Instead Of Turning
+        // All The Way To 0deg, It Stays At 180deg
         newState.optimize(Rotation2d.fromRotations(steerMotorRot));
 
-        // Calculate Speed Based on Error between the Target and Current Angle
+        // * Calculate Speed Based On Error Between The Target And Current Angle
         double steerSpeed = steerPID.calculate(steerMotorRot, newState.angle.getRotations());
 
-        // Pass Finalized Values to Motors
+        // * Pass Finalized Values/Instructions To Motors
         steerMotor.set(steerSpeed);
         driveMotor.set(newState.speedMetersPerSecond);
     }
 
-    // Set Drive Motor Speed
+    // * Sets Drive Motor Speed
     public void setDriveSpeed(double driveSpeed) {
         driveMotor.set(driveSpeed);
     }
 
-    // Zero Out Motor Encoders to Correct Alignment
+    // * Zeros Out Motor Encoders to Correct Alignment
     public void resetEncoders() {
         steerEncoder.setPosition(0);
         driveEncoder.setPosition(0);
     }
 
-    // Get Distance Traveled for Auto
+    // * Get Distance Traveled For Auto
     public double getEncoderDistance() {
         return driveEncoder.getPosition();
     }
